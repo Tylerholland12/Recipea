1fc49796b4302b3b00334bcd464cd914d51f475f
Tyler_Holland <tyler.holland@students.makeschool.com>
2021-03-08 22:48:00 -0600

    [update]

--- controllers/user.js
+++ controllers/user.js
@@ -20,20 +20,19 @@ module.exports = (app) => {
     body('password').isString().isStrongPassword(),
     body('email').isEmail(),
     body('publicRecipe').custom(isrecipe),
-    // eslint-disable-next-line consistent-return
+    // 
     (req, res) => {
       if (!req.user === null) {
         return res.status(401).send({ message: 'Not Authorised' });
       }
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
-        // eslint-disable-next-line no-console
+        // 
         console.log(errors);
         return res.status(400).render('invalid');
       }
       User
         .findOne({ username: req.body.username })
-        // eslint-disable-next-line consistent-return
         .then((foundUser) => {
           if (foundUser === null) {
             const user = User({
@@ -45,13 +44,11 @@ module.exports = (app) => {
             user
               .save()
               .then((savedUser) => {
-                // eslint-disable-next-line no-underscore-dangle
                 const token = jwt.sign({ _id: savedUser._id }, process.env.SECRET, { expiresIn: '60 days' });
                 res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
                 return res.redirect('/');
               })
               .catch((err) => {
-                // eslint-disable-next-line no-console
                 console.log(err);
                 return res.status(400).render('invalid');
               });
@@ -60,7 +57,6 @@ module.exports = (app) => {
           }
         })
         .catch((err) => {
-          // eslint-disable-next-line no-console
           console.log(err);
         });
     },
@@ -85,13 +81,11 @@ module.exports = (app) => {
   );
   app.put(
     '/user/update',
-    // eslint-disable-next-line consistent-return
     (req, res) => {
       if (req.user === null) {
         return res.redirect('/');
       }
       User
-        // eslint-disable-next-line no-underscore-dangle
         .findOneAndUpdate({ _id: req.user._id }, {
           username: req.body.username,
           password: req.body.password,
@@ -99,7 +93,6 @@ module.exports = (app) => {
           publicEthAddress: req.body.publicEthAddress,
         })
         .then(() => res.render('user'))
-        // eslint-disable-next-line no-console
         .catch((err) => console.log(err));
     },
   );
@@ -107,10 +100,8 @@ module.exports = (app) => {
     '/user/delete',
     (req, res) => {
       User
-        // eslint-disable-next-line no-underscore-dangle
         .findByIdAndDelete(req.user._id, (err) => {
           res.clearCookie('nToken');
-          // eslint-disable-next-line no-console
           console.log(err);
           return res.redirect('/');
         });
@@ -136,10 +127,9 @@ module.exports = (app) => {
           .then((foundUser) => {
             foundUser.comparePassword(req.body.password, (err, isMatch) => {
               if (!isMatch) {
-                // Password does not match
+                // Password doesnt match
                 return res.status(401).send({ message: 'Wrong Username or password' });
               }
-              // eslint-disable-next-line no-underscore-dangle
               const token = jwt.sign({ _id: foundUser._id }, process.env.SECRET, { expiresIn: '60 days' });
               res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
               return res.redirect('/');
