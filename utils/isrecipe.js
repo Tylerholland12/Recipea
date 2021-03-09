4c499a54cf016d21b4c3b539f878f187ad78efe5
Tyler_Holland <tyler.holland@students.makeschool.com>
2021-03-08 21:32:34 -0600

    [update] added comments

--- utils/isrecipe.js
+++ utils/isrecipe.js
@@ -1,5 +1,7 @@
+// Import spoonacular
 const recipe = require('spoonacular_api');
 
+// check to see if recipe exists
 module.exports = (address) => {
     if (!recipe.utils.recipe(address)) {
         throw new Error("incorrect recipe name");
